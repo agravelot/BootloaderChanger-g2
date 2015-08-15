0eb91e3a608d23ff837b849debe2812f3eabff87
Nevax <antoine.gravelot@hotmail.fr>
2015-08-15 17:38:32 +0200

    Update Build all.cmd
    fix variant

--- Build all.cmd
+++ Build all.cmd
@@ -5,55 +5,55 @@ set /p VERSION=Entrer la version :
 set VARIANT=d800
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=d801
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=d802
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=d803
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=d805
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=d806
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=F320K
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=F320L
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=F320S
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=LS980
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 
 set VARIANT=VS980
 echo %VARIANT% :
 7z a -r -tzip -mx9 "%MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
-md5 %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-d800-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
+md5 %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip >> %MEGADIR%\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.md5sum
 PAUSE
