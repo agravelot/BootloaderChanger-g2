b9202cc16e6ffbce6686a0c375ecd5b2448fa6e3
Nevax <antoine.gravelot@hotmail.fr>
2015-08-17 03:09:03 +0200

    Update Build all.cmd
    MD5 lower case letter -l
    MD5 no input file path -n

--- Build all.cmd
+++ Build all.cmd
@@ -4,67 +4,78 @@ set /p VERSION=Entrer la version :
 
 set VARIANT=d800
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=d801
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=d802
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=d803
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=d805
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=d806
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=F320K
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=F320L
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=F320S
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=LS980
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 
 set VARIANT=VS980
 echo %VARIANT% :
+
 7z a -r -tzip -mx9 "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" "%GITHUBDIR%\BootloaderChanger-g2\BootloaderChanger-%VARIANT%-nevax-\*" -xr!BootloaderChanger-*.* -xr!"-v- DOWNLOAD LATEST .ZIP FILE -v-"
 ping 127.0.0.1 -n 6 > nul
-md5.exe "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
+md5.exe -l -n "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip" >> "%MEGADIR%\Android\MOD\Bootloader Changer\BootloaderChanger-%VARIANT%-nevax-\BootloaderChanger-%VARIANT%-nevax-%VERSION%.zip.md5"
 PAUSE
